33bb6909ac57c3a505228954df02098737934072
brahim024 <ifninos168@gmail.com>
2020-12-13 14:20:50 +0100

    add binary search of simple list

--- binary-search/bs.py
+++ binary-search/bs.py
@@ -1,2 +1,18 @@
-low=0
-high=len(list)-1
+def binary_search(list,item):
+	low=0
+	high=len(list)-1
+	while low <= high:
+		mid=(low+high)
+		guess=list[mid]
+		if guess== item:
+			return mid
+		if guess > item:
+			high =mid-1
+		else:
+			high=mid +1
+	return None
+
+my_list=[-1,0,1,3,5,7,8]
+
+print(binary_search(my_list,3))
+print(binary_search(my_list,-1))
